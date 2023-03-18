55ff48363d5938d375ae9fda10c492081fd64008
ShakeZpear <meherss@uio.no>
2023-03-18 11:50:04 +0100

    uploead

--- Longest Common Prefix/Solution.java
+++ Longest Common Prefix/Solution.java
@@ -1,19 +1,37 @@
+import java.util.Arrays;
 
 class Solution {
-    
-    
     public static String longestCommonPrefix(String[] strs) {
-
-        return null;
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+        char[] firstARR = first.toCharArray();
+        char[] lastARR = last.toCharArray();
+        String returnString = "";
+        for (int i = 0; i < first.length(); i++) {
+            char firstLetter = firstARR[i];
+            char lastLetter;
+            try {
+                lastLetter = lastARR[i];
+            } catch (Exception e) {
+                break;
+            }
+            if(firstLetter == lastLetter){
+                returnString = returnString + firstLetter;
+            }
+            else{
+                break;
+            }
+        }
+        return returnString;
     }
 
 
-
+    /*
     public static void main(String[] args) {
-        String[] in = {"flower","flow","flight", "noe", "annet", "sjekk"};
-        longestCommonPrefix(in);
-
-
+        String[] in = {"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(in));
         
     }
+     */
 }
